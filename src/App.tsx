d8efb4ece5191cfe49237e892fb79133e2639dc7
ishraaq2001parvez <i4ishraaq@gmail.com>
2025-10-19 17:13:29 +0530

    added simple non-persistent login

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,15 @@ import './App.css'
 import User from './components/User'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <User></User>
-      </div>
-    </>
-  )
+    const [count, setCount] = useState(0)
+    
+    return (
+        <>
+        <div>
+            <User></User>
+        </div>
+        </>
+    )
 }
 
 export default App
